715de864f53e89982e6e69dcb6727890b2294cbc
Lê Quang Huy <lequanghuylc@gmail.com>
2026-03-17 16:30:35 +0700

    add .nvmrc and pm2 config

---
+++ expo-updates-server/pm2.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+      {
+        name: "personify-micro-apis",
+        script: "npm",
+        args: "start",
+        watch: false,
+        env: {
+          PORT: 3427
+        }
+      }
+    ]
+  }
+  
